24e8ffb4be4c2dd71b6a29ecd15df21936ad66ac
Artus Kolanowski <artus@ionoi.net>
2015-03-07 15:35:04 +0100

    Refactors the whole task
    This refactors the whole task by applying the DRY paradigma. Also it
    removes the override of options through the command line which makes no
    sense in my opinion.

--- tasks/phpcs.js
+++ tasks/phpcs.js
@@ -1,6 +1,6 @@
 /*
- * grunt-phpunit
- * https://github.com/SaschaGalley/grunt-phpunit
+ * grunt-phpcs
+ * https://github.com/SaschaGalley/grunt-phpcs
  *
  * Copyright (c) 2013 Sascha Galley
  * http://xash.at
@@ -10,11 +10,65 @@
 
 module.exports = function(grunt) {
 
-    // Internal lib.
-    var phpcs = require('./lib/phpcs').init(grunt);
+    var path = require('path'),
+        exec = require('child_process').exec;
+    
+    var command = {
+            flags: {
+                verbose: 'v',
+                showSniffCodes: 's'
+            },
+            options: {
+                errorSeverity: 'error-severity',
+                extensions: 'extensions',
+                ignore: 'ignore',
+                report: 'report',
+                reportFile: 'report-file',
+                severity: 'severity',
+                standard: 'standard',
+                warningSeverity: 'warning-severity',
+                tabWidth: 'tab-width'
+            }
+        },
+        defaults = {
+            bin: 'phpcs',
+            report: 'full',
+            maxBuffer: 200*1024
+        },
+        done = null;
 
-    grunt.registerMultiTask('phpcs', 'Run phpunit', function() {
-        phpcs.setup(this);
-        phpcs.run();
+    grunt.registerMultiTask('phpcs', 'Run PHP Code Sniffer', function() {
+        var done = null,
+        options = this.options(defaults),
+        execute = path.normalize(options.bin),
+        files = [].concat.apply([], this.files.map(function(mapping) { return mapping.src; })).sort();
+    
+        for (var flag in command.flags) {
+            if (options[flag] === true) {
+                execute += ' -' + command.flags[flag];
+            }
+        }
+        
+        for (var option in command.options) {
+            if (options[option] !== undefined) {
+                execute += ' --' + command.options[option] + '=' + options[option];
+            }
+        }
+        
+        files = files.filter(function(file, position) { return !position || file !== files[position - 1]; });
+        
+        execute += ' ' + '"' + files.join('" "') + '"';
+    
+        grunt.log.writeln('Checking ' + files.length + ' file' + (files.length === 1 ? '' : 's') + '...');
+        grunt.verbose.writeln('Executing: ' + execute);
+        
+        done = this.async();
+    
+        exec(execute, {maxBuffer: options.maxBuffer}, function(err, stdout, stderr) {
+            /* jshint -W030 */
+            stdout && grunt.log.write(stdout);
+            err && !options.ignoreExitCode && grunt.fatal(err);
+            done();
+        });
     });
 };
